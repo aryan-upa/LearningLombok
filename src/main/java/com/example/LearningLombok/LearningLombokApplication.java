1a2e10ec86a81a13d8e7376e5dad368b631c54ba
Aryan Upadhyay <aryan.offc@gmail.com>
2022-04-12 19:25:18 +0530

    Initial Commit

---
+++ src/main/java/com/example/LearningLombok/LearningLombokApplication.java
@@ -0,0 +1,13 @@
+package com.example.LearningLombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningLombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningLombokApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LearningLombok/LearningLombokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LearningLombok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningLombokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
